b0470e8a5ca89d756972f1a2d85fcad88206fb52
Stephen Adams <tsadams@gmail.com>
2019-08-14 11:50:52 -0400

    Add a value_deserializer to consumer
    This sets the deserializer so it automatically json.loads all the
    messages. You don't have to do it in the loop with this.
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

--- mq/kafka_consumer.py
+++ mq/kafka_consumer.py
@@ -5,9 +5,11 @@ from kafka import KafkaConsumer
 from utils import config
 
 
-
 def init_consumer():
-    consumer = KafkaConsumer(config.TOPIC, group_id=config.KAFKA_GROUP, 
-    bootstrap_servers=config.BOOTSTRAP_SERVERS)
+    consumer = KafkaConsumer(config.TOPIC,
+                             group_id=config.KAFKA_GROUP,
+                             bootstrap_servers=config.BOOTSTRAP_SERVERS,
+                             value_deserializer=lambda m: json.loads(m.decode("utf-8"))
+                             )
 
     return consumer
