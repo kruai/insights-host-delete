d0e63fb140ad42b4397a60ef25a99d201ec56c5c
Alec Cohan <44471274+aleccohan@users.noreply.github.com>
2019-08-14 11:25:37 -0400

    Took out Dummy test data
    Deleted dummy data used to test app on local machine.

--- utils/config.py
+++ utils/config.py
@@ -15,10 +15,3 @@ TOPIC = os.environ.get("KAFKA_TOPIC", "platform.inventory.events")
 KAFKA_GROUP = os.environ.get("KAFKA_GROUP", "inventory")
 BOOTSTRAP_SERVERS = os.environ.get("KAFKA_BOOTSTRAP_SERVERS", "localhost:29092")
 LEGACY_URL = "https://access.redhat.com/r/insights/v1/systems"
-
-
-'''
-TESTING AUTHENTICATION DATA BASED ON VM
-'''
-INSIGHTS_ID = '389fb722-1fd1-4a60-bd3e-0f7bb23e0d5b'
-ACCOUNT = '540155'
