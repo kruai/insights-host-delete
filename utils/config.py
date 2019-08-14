e5306bfa6170c13dbe726b6da31dab3a9c715e60
Alec Cohan <alcohan@redhat.com>
2019-08-14 11:12:43 -0400

    Removing dummy data and non usefull pipfile entries

--- utils/config.py
+++ utils/config.py
@@ -20,7 +20,5 @@ LEGACY_URL = "https://access.redhat.com/r/insights/v1/systems"
 '''
 TESTING AUTHENTICATION DATA BASED ON VM
 '''
-USERNAME = "rhn-support-alcohan"
-PASSWORD = "Qg@8dN91pFyAED%bxO"
 INSIGHTS_ID = '389fb722-1fd1-4a60-bd3e-0f7bb23e0d5b'
 ACCOUNT = '540155'
