a21fa0824c152e3864ea1571e2e84d33d8ca045e
Stephen Adams <tsadams@gmail.com>
2019-08-14 11:48:19 -0400

    Break out logging
    - configure ability to log to cloudwatch
    - add config log printing when launch
    - initialize logging function to get the whole thing going
    
    Signed-off-by: Stephen Adams <tsadams@gmail.com>

---
+++ utils/host_delete_logging.py
@@ -0,0 +1,44 @@
+import logging
+
+import os
+import sys
+
+import watchtower
+
+from logstash_formatter import LogstashFormatterV1
+from boto3.session import Session
+
+from utils import config
+
+
+def config_cloudwatch(logger):
+    CW_SESSION = Session(aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+                         aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY,
+                         region_name=config.AWS_REGION_NAME)
+    cw_handler = watchtower.CloudWatchLogHandler(boto3_session=CW_SESSION,
+                                                 log_group=config.LOG_GROUP,
+                                                 stream_name=config.NAMESPACE)
+    cw_handler.setFormatter(LogstashFormatterV1())
+    logger.addHandler(cw_handler)
+
+
+def initialize_logging():
+    kafkalogger = logging.getLogger("kafka")
+    kafkalogger.setLevel("ERROR")
+    if any("OPENSHIFT" in k for k in os.environ):
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(LogstashFormatterV1())
+        logging.root.setLevel(config.LOG_LEVEL)
+        logging.root.addHandler(handler)
+    else:
+        logging.basicConfig(
+            level=config.LOG_LEVEL,
+            format="%(threadName)s %(levelname)s %(name)s - %(message)s"
+        )
+
+    logger = logging.getLogger(config.APP_NAME)
+
+    if (config.AWS_ACCESS_KEY_ID and config.AWS_SECRET_ACCESS_KEY):
+        config_cloudwatch(logger)
+
+    return logger
